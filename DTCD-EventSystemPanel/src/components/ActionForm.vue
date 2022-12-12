0efafddb5b999394b7ee53d7d324f66394eaef87
Alexandr-Bragin <abragin@isgneuro.com>
2022-12-12 23:08:48 +0900

    Replaced attribute

--- DTCD-EventSystemPanel/src/components/ActionForm.vue
+++ DTCD-EventSystemPanel/src/components/ActionForm.vue
@@ -60,13 +60,13 @@
         <base-textarea
           label="Функция"
           required
-          data-autoheight
           theme="resize_off"
           placeholder="Тело JS-функции"
           size="big"
           :value="actionFormData.body"
           @input="(e) => (actionFormData.body = e.target.value)"
           :invalid="$v.actionFormData.body.$dirty && $v.actionFormData.body.$invalid"
+          data-autoheight
         ></base-textarea>
       </div>
     </div>
